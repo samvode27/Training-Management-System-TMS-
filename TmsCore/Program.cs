679ec287923b4b1ee33be9890e5864a86e7c69c9
Samuel Setarige <setarigesamuel@gmail.com>
2026-05-25 18:05:41 +0300

    Implement precise financial calculations using decimal

--- TmsCore/Program.cs
+++ TmsCore/Program.cs
@@ -1,17 +1,33 @@
-﻿System.Console.WriteLine("//////// This variable may be null ////////");
+﻿// System.Console.WriteLine("//////// This variable may be null ////////");
 
-string? region = null;
+// string? region = null;
 
-Console.WriteLine(region?.ToUpper());
+// Console.WriteLine(region?.ToUpper());
 
-System.Console.WriteLine("//////// Null Coalescing(If null, use fallback) ////////");
+// System.Console.WriteLine("//////// Null Coalescing(If null, use fallback) ////////");
 
-string display = region ?? "Unassigned";
+// string display = region ?? "Unassigned";
 
-Console.WriteLine(display);
+// Console.WriteLine(display);
 
-System.Console.WriteLine("//////// Null Coalescing Assignment(Assign only if currently null.) ////////");
+// System.Console.WriteLine("//////// Null Coalescing Assignment(Assign only if currently null.) ////////");
 
-region ??= "Addis Ababa";
+// region ??= "Addis Ababa";
 
-Console.WriteLine(region);
+// Console.WriteLine(region);
+
+
+
+
+
+System.Console.WriteLine("//////////// double stores values in binary approximation. ////////////");
+double grant = 1999.99;
+double total = grant * 100000;
+
+Console.WriteLine(total);
+
+System.Console.WriteLine("//////////// Correct: Use decimal ////////////");
+decimal money = 1999.99m;
+decimal final = money * 100000m;
+
+Console.WriteLine(final);
