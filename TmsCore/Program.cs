07f35720f7d8bb3cc12906b3d983635c29f678aa
Samuel Setarige <setarigesamuel@gmail.com>
2026-05-25 17:56:56 +0300

    Add nullable reference handling and null safety examples

--- TmsCore/Program.cs
+++ TmsCore/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("//////// This variable may be null ////////");
+
+string? region = null;
+
+Console.WriteLine(region?.ToUpper());
+
+System.Console.WriteLine("//////// Null Coalescing(If null, use fallback) ////////");
+
+string display = region ?? "Unassigned";
+
+Console.WriteLine(display);
+
+System.Console.WriteLine("//////// Null Coalescing Assignment(Assign only if currently null.) ////////");
+
+region ??= "Addis Ababa";
+
+Console.WriteLine(region);
--- TmsCore/obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ TmsCore/obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a76fc4b6ac4387f5270bfa2d347ca75c0ffb01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af9469ad418725885734ef29473a0102aa69e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
